86001d71921e0028148c18fd0f5340a0dae692a9
wambui <wambuistephen@zetech.ac.ke>
2024-10-16 15:18:18 +0300

    git project emobilis

---
+++ pythonguru.py
@@ -0,0 +1,17 @@
+name = "stephen"
+
+age = 26
+
+actual_age = 26.2
+math = 5** 7 + 6/9
+result = age + actual_age
+print(result)
+
+order = input()
+price = 25
+num = input("What is your name\n ")
+quantity= input("what is your coffee quantity\n")
+total_price = price * int(quantity)
+print("Thank you " + name + " your total is: Ksh" + str(total_price))
+print("Good " +num+ " wait am getting your " + quantity + " " + order + " right away")
+
